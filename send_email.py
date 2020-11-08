19b11a403148d6512e8689c77379eb36139a6f49
oluoluo <hustly123@gmail.com>
2020-11-08 16:28:10 +0800

    add send mail

--- send_email.py
+++ send_email.py
@@ -7,7 +7,7 @@ from smtplib import SMTP_SSL
 # 第三方 SMTP 服务
 mail_host="smtp.qq.com"  #设置服务器
 mail_user="your qq email"    #用户名
-mail_pass="your key"   #口令
+mail_pass="your key"   #SMTP授权码，搜索：qq邮箱smtp设置
 sender = 'your qq email'
 receivers = ['your qq email']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 def send_mail(message):
@@ -21,6 +21,6 @@ def send_mail(message):
             smtp.ehlo(mail_host)
             smtp.login(mail_user,mail_pass)
             smtp.sendmail(sender, receivers, message.as_string())
-            print "邮件发送成功"
+            print("邮件发送成功")
         except smtplib.SMTPException:
-            print "Error: 无法发送邮件"
+            print("Error: 无法发送邮件")
